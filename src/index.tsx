daf38ba2ca884f3925c32b0500c07a47208602dc
Raihan Kulmagambetove <raikhankull@gmail.com>
2021-11-17 13:00:29 +0300

    app setup

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/app';
+
+import 'styled/global.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
